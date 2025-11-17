58238eac56d513aa458ea1ab997dec99dfad1ba6
MalvMoonlight <dwolf.mk@gmail.com>
2025-11-17 19:10:57 +0100

    update ch2l3

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.vernum}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    x=1
+
+    while x==1:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()   
 
 if __name__ == "__main__":
     main()
