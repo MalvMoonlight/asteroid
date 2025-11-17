13612ce04803e97ef286ae211ea1775b5689a00c
MalvMoonlight <dwolf.mk@gmail.com>
2025-11-17 18:56:45 +0100

    "Asteroide"

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.vernum}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
